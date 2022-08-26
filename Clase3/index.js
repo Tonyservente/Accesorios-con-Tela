63cf83c470fe56d0a0ff1bd1d768a30bfc49dbcf
tonyservente <anthonyservente@gmail.com>
2022-08-25 22:21:52 -0300

    clase 3

---
+++ Clase3/index.js
@@ -0,0 +1,8 @@
+for (let turno = 1; turno <=10; turno++){
+    let nombre = prompt("Bienvenido ingrese su nombre para asignar turno y ser atendido, Gracias!");
+    let mensaje = `turno #${turno} Nombre: ${nombre}`;
+    alert (mensaje)
+}
+
+alert ("Se acabaron los turnos");
+
