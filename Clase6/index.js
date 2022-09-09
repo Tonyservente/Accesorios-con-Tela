54a20b3c408a85ca6faa8651db3fcc1b09efddb0
anthonyservente <anthonyservente@gmail.com>
2022-09-08 22:24:30 -0300

    Entrega primer avance del proyecto

--- Clase6/index.js
+++ Clase6/index.js
@@ -1,82 +1,209 @@
 function container() {
-
-function nuevoNombre() {
-    alert('Accesorios con tela')
-
-    let nombre = ""
-    while (nombre === '') {
-        nombre = prompt('Accesorios con tela\n\nIngrese su NOMBRE o presione en cancelar para SALIR').toUpperCase();
-    }
-    alert(`Bienvenido ${nombre}`);
-
-    return nombre;
-}
-
-let cliente = nuevoNombre();
-
-
-const productos = [
-    { nombre: "Moña", precio: 150, color:["rosa", "azul", "violeta", "lila", "gris"]},
-    { nombre: "Moña cherry", precio: 120, color:["negro", "violeta", "lila", "rojo"]},
-    { nombre: "Vincha trenzada", precio: 220, color:["verde", "violeta", "rojo", "azul"]},
-    { nombre: "Vincha Fancy", precio: 210, color:["rojo", "verde", "azul"]},
-    { nombre: "Scrunchie", precio: 100, color:["verde", "violeta", "rojo", "azul"]},
-    { nombre: "Scrunchie colero dream", precio: 150, color:["rosa", "azul", "violeta", "lila", "gris"]},
-];
-
-let carrito = []
-
-let seleccion = prompt("¿Desea agregar algun producto? si o no");
-
-while(seleccion != "si" && seleccion != "no"){
-    alert("Por favor ingrese si o no");
-    seleccion = prompt("¿Desea comprar? si o no");
-}
-
-if(seleccion == "si"){
-    alert(" A continuación se mostraran los productos que tenemos disponibles");
-    let todoslosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    console.log(todoslosProductos);
-    alert (todoslosProductos.join( "-"));
-} else if (seleccion == "no"){
-    alert ("Gracias por venir, hasta pronto!");
-}
-
-while ( seleccion != "no"){
-    let pedido = prompt("Agrega un producto a tu carrito: 1) Moña  $150 \n======\n 2) Moña cherry $120 \n======\n 3) Vincha Trenzada $220 \n======\n 4) Vincha Fancy $210 \n======\n 5) Scrunchie $100 \n======\n 6) Scrunchie colero dream $150");
-    let precio = 0;
-
-    productoElegido= productos.find(item => item.nombre=pedido);
+  
+    const productos = [{
+        id: 1,
+        nombre: 'Moña',
+        precio: 150,
+        cantidad: 300
+      },
+      {
+        id: 2,
+        nombre: 'Moña cherry',
+        precio: 120,
+        cantidad: 20
+      },
+      {
+        id: 3,
+        nombre: 'Vincha trenzada',
+        precio: 220,
+        cantidad: 30
+      },
+      {
+        id: 4,
+        nombre: 'Vincha Fancy',
+        precio: 210,
+        cantidad: 15
+      },
+      {
+        id: 5,
+        nombre: 'Scrunchie',
+        precio: 100,
+        cantidad: 50
+      },
+      {
+        id: 6,
+        nombre: 'Scrunchie colero dream',
+        precio: 150,
+        cantidad: 10
+      },
+    ];
+    
+     class Productos {
+        constructor(id, nombre, precio, cantidad){  
+          this.id = id;
+          this.nombre = nombre;
+          this.precio = precio;
+          this.cantidad = cantidad;
+        }
+      }
+    
+      const carritoComprar = [];
+      let validarObj;
+      let arraydeProductos = [];
+    
+      function validacion(nombre) {
+        let nuevaEntrada = prompt(nombre);
+        while (nuevaEntrada == '' || nuevaEntrada == null) {
+          alert('Error! - No se ingresaron datos');
+          nuevaEntrada = prompt(nombre);
+        }
+        return validarObj = nuevaEntrada;
+      }
+    
+      function menu() {
+        validacion(opciones);
+        parseInt(validarObj);
+        switch (validarObj) {
+          case "1":
+            menuLista();
+            break;
+          case "2":
+            salir(false);
+            break;
+          default:
+            alert("Error en el dato");
+            menu();
+            break;
+        }
+      }
+    
+      function menuLista() {
+        let mostrarProductos = '';
+        let a = 1;
+        productos.forEach((i) => {
+          mostrarProductos += (`${i.id} - ${i.nombre} \n        precio = ${i.precio} Pesos. ~ disponible ${i.cantidad} unidades.\n`);
+          a++;
+        });
+        let menuProductos = (`Agregue al carrito el producto deseado :\n\n${mostrarProductos}${a} - Ir al pago -->\n${a + 1} - Vaciar carrito <--`);
+        menuListaProd(menuProductos, a);
+      }
+    
 
     
-    if (productoElegido!== "undefined"){
-        let colorProducto= productoElegido.color;
-        let colores= colorProducto.join(" - ");
-        seleccion = prompt(`Colores disponibles: ${colores}`);
-        let  unidades = parseInt(prompt("Cuantas unidades desea agregar"));
-
-    carrito.push({ nombreVenta: productoElegido.nombre, unidadesVenta: unidades, precioVenta: productoElegido.precio});
-    console.log(carrito);
-    } else {
-        alert("No tenemos este producto")
+      function menuListaProd(menuProductos, a) {
+        validacion(menuProductos);
+        if (validarObj == a) {
+          if (carritoComprar.length == 0) {
+            alert('carrito Vacío');
+            menuLista();
+          } else {
+            mostrandoPedidos();
+          }
+        } else if (validarObj == a + 1) {
+          alert('Vaciando Carrito!');
+          let consulta = parseInt(prompt('Con tela joyeria\n\n ¿Desea continuar? :\n\n1 - menú\n2 - salir'));
+          while (consulta != 500 ) {
+            if (consulta == 1) {
+              menu();
+              break;
+            } else if (consulta == 2) {
+              salir(false);
+              break;
+            } else {
+              alert("Error en el dato");
+              consulta = parseInt(prompt('Con tela joyeria\n\n ¿Desea continuar? :\n\n1 - menú\n2 - salir'));
+            }
+          }
+        } else if (validarObj > 0 && validarObj < productos.length + 1) {
+          arryFiltroProducto();
+        } else {
+          alert('Error! - ingresa otra opción');
+          menuLista();
+        }
+      }
+     
+       function arryFiltroProducto() {
+        let cantidad = 1;
+        productos.map((productos) => {
+          if (productos.id == validarObj) {
+            arraydeProductos = {
+              id: productos.id,
+              titulo: productos.titulo,
+              precio: productos.precio,
+              cantidad: productos.cantidad,
+            };
+            return arraydeProductos;
+          }
+        });
+        agregandoalCarrito(arraydeProductos);
+      }
+    
+      function agregandoalCarrito(arraydeProductos) {
+        const acumulado = carritoComprar.some((elemento) => elemento.id == validarObj);
+        if (acumulado == false) {
+          const nuevoCarrito = new Productos(arraydeProductos);
+          carritoComprar.push(nuevoCarrito);
+          alert(`Con tela joyeria // CARRITO :\n\nAgregaste -- > ${arraydeProductos.nombre}`);
+        }
+        else {
+          carritoComprar.filter((elemento) => {
+            if (elemento.id == validarObj) {
+              elemento.cantidad++
+            }
+          });
+          alert(`Con tela joyeria // CARRITO :\n\nAgregaste --> ${arraydeProductos.nombre}`);
+        }
+        menuLista();
+      }
+    
+      function mostrandoPedidos() {
+        let mostrarCarrito = "";
+        let subTotal = 0;
+        total = 0;
+        carritoComprar.forEach((i) => {
+          subTotal = i.precio * i.cantidad;
+          total += subTotal;
+          mostrarCarrito += (`${i.nombre} / valor : ${i.precio} Pesos. / unds : ${i.cantidad}\ncosto x Productos ${subTotal} Pesos.\n\n`);
+        });
+        alert(`Con tela joyeria // CARRITO :\n\n${mostrarCarrito}\nTotal a Pagar ${total} Pesos.`);
+        cobrar();
+      }  
+    
+      function cobrar() {
+        validacion(cash);
+        parseInt(validarObj);
+        switch (validarObj >= total) {
+          case true:
+            alert(`Con tela joyeria \n\nTu vuelto es ${validarObj - total} Pesos.`);
+            salir(true);
+            break;
+          case false:
+            alert(`Con tela joyeria \n\n'Error!' Monto insuficiente`);
+            cobrar();
+            break;
+          default:
+            alert('Error! - ingresa otra opción');
+            cobrar();
+            break;
+        }
+      }
+    
+      function salir(mensaje) {
+        if (mensaje == false) {
+          alert('Con tela joyeria \n\nGracias Por tu Visita. Te Esperamos Pronto!');
+          return;
+        } else {
+          alert('Con tela joyeria\n\nPuedes retirar tu pedido en Despacho!');
+          alert('Con tela joyeria\n\nGracias Por tu Compra, te Esperamos Pronto');
+          return;
+        }
+      }
+    
+       let nombre = 'Con tela joyeria\n\nIngrese su nombre :';
+       let opciones = 'Con tela joyeria\n\nIngrese una opcion: \n\n1 - Comprar productos\n2 - Salir';
+       let cash = 'Con tela joyeria\n\n Ingrese efectivo :';
+       validacion(nombre);
+       alert('Con tela joyeria\n\nBienvenido(a)  ' + validarObj.toUpperCase());
+       menu();
+    
     }
-
-
-    seleccion = prompt("¿Desea continuar con la compra?");
-
-
-    if (seleccion === "no"){
-        alert("Gracias por su compra! hasta pronto");
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal}), unidades: ${carritoFinal.unidades}),
-            total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
-        })
-    break;
-    }
-}
-
-const total = carrito.reduce((acc, el) => {acc + (el.precio * el.unidades)}, 0);
-alert(`el total a pagar de su compra es de: ${total}`);
-
-}
-
